09be5cc55e94239c07a498c62a2f678dc381487e
Benedictus See <78637682+benedictusdps@users.noreply.github.com>
2021-08-07 21:01:09 +0700

    Delete 1. Simple Band Name Generator directory

--- 1. Simple Band Name Generator/main.py
+++
@@ -1,7 +0,0 @@
-# Print a welcome sign
-print("Hello and welcome to the band name generator.")
-# Ask for the user's input
-city = input("What is the name of the city that you grew up in?\n")
-pet = input("What is the name of your pet?\n")
-# Output the user's input as their band name
-print("Your band name could be the " + city + " " + pet + ".")
